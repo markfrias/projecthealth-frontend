eef63afa97b239183f0eda91a4e3211d4e690d08
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-02-07 11:57:48 +0800

    Add profile component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 const Login = () => {
     return (
         <div>
             <button className="button-primary">Login</button>
+            <Button variant="contained">Contained</Button>
+            <button className='btn btn-primary'>Hello</button>
+
         </div>
     );
 }
