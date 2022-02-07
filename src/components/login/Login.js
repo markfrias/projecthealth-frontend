c6c1f7177c4ade607898d3e13d4b06abae93607c
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-02-07 11:09:14 +0800

    Add SCSS file structure

---
+++ src/components/login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <button className="button-primary">Login</button>
+        </div>
+    );
+}
+
+export default Login;
