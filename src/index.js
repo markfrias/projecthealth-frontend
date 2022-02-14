732f6496a8e21b9ea6f1f61083adfc1f2d831471
Jared Ibañez <54757792+JaredIbanez69@users.noreply.github.com>
2022-02-14 16:25:22 +0800

    add onboarding page 1 to 5

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./components/landing/Landing";
+import Onboarding1 from "./components/post-login/Onboarding1";
+import Onboarding2 from "./components/post-login/Onboarding2";
+import Onboarding3 from "./components/post-login/Onboarding3";
+import Onboarding4 from "./components/post-login/Onboarding4";
+import Onboarding5 from "./components/post-login/Onboarding5";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +18,11 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="" element={<Landing />} />
+          <Route path="Onboarding1" element={<Onboarding1 />} />
+          <Route path="Onboarding2" element={<Onboarding2 />} />
+          <Route path="Onboarding3" element={<Onboarding3 />} />
+          <Route path="Onboarding4" element={<Onboarding4 />} />
+          <Route path="Onboarding5" element={<Onboarding5 />} />
         </Route>
       </Routes>
     </BrowserRouter>
