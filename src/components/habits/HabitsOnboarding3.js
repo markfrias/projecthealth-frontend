9ef33f04dcea35ca9dddc15d9fd29dc572b77f74
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-03-03 23:36:44 +0800

    Add control for habit creation and autofill

--- src/components/habits/HabitsOnboarding3.js
+++ src/components/habits/HabitsOnboarding3.js
@@ -1,8 +1,14 @@
-import { Container } from "@mui/material";
-import React from "react";
+import { Autocomplete, Container } from "@mui/material";
+import React, { useEffect } from "react";
 import { FormGroup, Button, TextField } from "@mui/material";
 
-const HabitsOnboarding3 = () => {
+const HabitsOnboarding3 = (props) => {
+    // Test code
+
+    useEffect(() => {
+        console.log(props.habitsState);
+    }, [props.habitsState]);
+
     return (
         <Container maxWidth="md" sx={{
             display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center", height: "100vh"
@@ -10,26 +16,54 @@ const HabitsOnboarding3 = () => {
             <div className="header" style={{ marginLeft: 10 }}>
 
                 <p>Create a habit by filling all the required fields below.</p>
-                <p>*required filleds</p>
+                <p><i>*required fields</i></p>
             </div>
             <FormGroup style={{ marginBottom: 200, marginLeft: 10 }}>
+
                 <h2 style={{ fontSize: 18 }}>Habit Name*</h2>
                 <TextField
                     helperText="e.g., Eat a fruit every breakfast"
                     id="demo-helper-text-misaligned"
                     label="Name"
+                    name="habitName"
+                    value={props.habitsState.habitName}
+                    onChange={props.handleChange}
                 />
                 <h2 style={{ fontSize: 18 }}>Habit Description*</h2>
                 <TextField
                     helperText="e.g., Eat a fruit every day for breakfast to increase fruit consumption."
                     id="demo-helper-text-misaligned"
                     label="Description"
+                    name="habitDescription"
+                    value={props.habitsState.habitDescription}
+                    onChange={props.handleChange}
                 />
                 <h2 style={{ fontSize: 18 }}>Goal Category</h2>
                 <TextField
                     helperText="e.g., Eat healthier."
                     id="demo-helper-text-misaligned"
                     label="Category"
+                    name="habitCategory"
+                    value={props.habitsState.goalCategory}
+                    onChange={props.handleChange}
+                />
+
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={props.goalOptions}
+                    value={props.goalCategoryValue}
+                    onChange={(event, newValue) => {
+                        props.setGoalCategoryValue(newValue)
+                    }}
+                    inputValue={props.habitsState.goalCategoryInputValue}
+                    onInputChange={(event, newInputValue) => {
+                        props.setHabitsState({
+                            ...props.habitsState,
+                            goalCategoryInputValue: newInputValue
+                        })
+                    }}
+                    renderInput={(params) => <TextField {...params} label="Goal Category" />}
                 />
 
 
--- src/components/registration/Habits.js
+++ src/components/registration/Habits.js
@@ -7,13 +7,47 @@ import HabitsOnboarding4 from '../habits/HabitsOnboarding4';
 
 const Habits = () => {
     const initialState = {
-        habitsForSubmission: []
+        habitsForSubmission: [],
+        newHabit: {
+            habitName: "",
+            habitDescription: "",
+            goalCategory: "",
+        },
+        habitName: "",
+        habitDescription: "",
+        goalCategory: "",
+        goalAutocomplete: [{ label: "Kim Mingyu" }],
+        goalCategoryValue: { label: "Kim Mingyu" },
+        goalCategoryInputValue: ""
     }
-
+    const options = [
+        { label: "Kim Mingyu", id: 1 },
+        { label: "Seungcheol", id: 2 }
+    ]
 
     // Habits pages state
     const [habitsState, setHabitsState] = useState(initialState);
     const [prepChecked, setPrepChecked] = useState([1]);
+    const [goalOptions, setGoalOptions] = useState(options);
+    const [goalCategoryValue, setGoalCategoryValue] = useState(goalOptions[0])
+
+
+
+
+    // Handle goal category autocomplete value
+    const handleGoalCatValue = (event) => {
+        setHabitsState({
+            ...habitsState,
+            goalCategoryValue: event.target.value
+        })
+    }
+
+    const handleGoalCatInputValue = (newInputValue) => {
+        setHabitsState({
+            ...habitsState,
+            goalCategoryInputValue: newInputValue
+        })
+    }
 
     const handlePrepToggle = (value) => () => {
         const currentIndex = prepChecked.indexOf(value);
@@ -28,16 +62,28 @@ const Habits = () => {
         setPrepChecked(newChecked);
     };
 
+    const handleChange = (event) => {
+        const target = event.target;
+
+        setHabitsState({
+            ...habitsState,
+            [target.name]: target.value
+        })
+    }
+
+    // Test function
+    /*
     useEffect(() => {
         console.log(prepChecked.indexOf(0))
 
     }, []);
+    */
 
     return (
         <Routes>
             <Route path="1" element={<HabitsOnboarding1 />} />
-            <Route path="2" element={<HabitsOnboarding2 handleToggle={handlePrepToggle} checked={prepChecked} habitsState={habitsState} />} />
-            <Route path="3" element={<HabitsOnboarding3 />} />
+            <Route path="2" element={<HabitsOnboarding2 handleToggle={handlePrepToggle} checked={prepChecked} habitsState={habitsState} handleGoalCatInputValue={handleGoalCatInputValue} handleGoalCatValue={handleGoalCatValue} goalCategoryValue={goalCategoryValue} setGoalCategoryValue={setGoalCategoryValue} />} />
+            <Route path="3" element={<HabitsOnboarding3 handleChange={handleChange} habitsState={habitsState} setHabitsState={setHabitsState} goalCategoryValue={goalCategoryValue} setGoalCategoryValue={setGoalCategoryValue} goalOptions={goalOptions} />} />
             <Route path="4" element={<HabitsOnboarding4 />} />
 
         </Routes>
