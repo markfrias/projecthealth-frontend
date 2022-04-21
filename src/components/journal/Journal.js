91719f607300cc1139579fa6782e8330d565dee1
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-04-21 17:27:43 +0800

    Don't show streak until it's loaded

--- src/components/journal/Journal.js
+++ src/components/journal/Journal.js
@@ -289,27 +289,35 @@ const Journal = () => {
 
                                     currentStreak === undefined ?
 
-                                        <Grid item container direction="row" xs={12} mb={2} spacing={2} alignItems="center">
-
-                                            <Grid item xs={4} >
-                                                <img src="https://img.icons8.com/emoji/96/000000/unamused-face.png" alt="Unimpressed emoji" width="100%" height="auto" />                                            </Grid>
-
-                                            <Grid item mb={2} xs={8}>
-                                                <Typography sx={{ width: '100%' }} variant="subtitle1" component="h1">You have not fed your pet in a while. Your pet seems unimpressed.</Typography>
-                                            </Grid>
-
+                                        <Grid item container direction="column" sx={{ height: '100vh', px: '1em' }} alignItems="center" justifyContent="center">
+                                            <CircularProgress variant='indeterminate' sx={{ mb: '2em' }} />
+                                            <Typography variant="p">Loading content</Typography>
                                         </Grid>
                                         :
-                                        <Grid item container direction="row" xs={12} mb={2} spacing={2} alignItems="center">
+                                        currentStreak > 0 ?
+                                            <Grid item container direction="row" xs={12} mb={2} spacing={2} alignItems="center">
 
-                                            <Grid item xs={4} >
-                                                <img alt="Party popper" src={require('../../assets/img/3d-confetti.png')} width="100%" height="auto" />
+                                                <Grid item xs={4} >
+                                                    <img alt="Party popper" src={require('../../assets/img/3d-confetti.png')} width="100%" height="auto" />
+                                                </Grid>
+                                                <Grid item mb={2} xs={8}>
+                                                    <Typography sx={{ width: '100%' }} variant="subtitle1" component="h1">You fed Pobi for <strong>{currentStreak} {currentStreak > 1 ? "days" : "day"}</strong> straight. Log every day to increase your streak and strengthen your gotchi.</Typography>
+                                                </Grid>
+
                                             </Grid>
-                                            <Grid item mb={2} xs={8}>
-                                                <Typography sx={{ width: '100%' }} variant="subtitle1" component="h1">You fed Pobi for <strong>{currentStreak} {currentStreak > 1 ? "days" : "day"}</strong> straight. Log every day to increase your streak and strengthen your gotchi.</Typography>
+                                            :
+                                            <Grid item container direction="row" xs={12} mb={2} spacing={2} alignItems="center">
+
+                                                <Grid item xs={4} >
+                                                    <img src="https://img.icons8.com/emoji/96/000000/unamused-face.png" alt="Unimpressed emoji" width="100%" height="auto" />                                            </Grid>
+
+                                                <Grid item mb={2} xs={8}>
+                                                    <Typography sx={{ width: '100%' }} variant="subtitle1" component="h1">You have not fed your pet in a while. Your pet seems unimpressed.</Typography>
+                                                </Grid>
+
                                             </Grid>
 
-                                        </Grid>
+
                             }
 
                         </Grid>
